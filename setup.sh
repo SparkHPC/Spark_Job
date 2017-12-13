97f0043bcf184ec4623307299cccb8f1cc0d9123
Xiao-Yong Jin <xjin@anl.gov>
2017-12-13 16:47:33 +0000

    work around SoftEnv's cleverness

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 export WORKING_DIR
 
 [[ $WORKING_DIR/soft_env.cache.sh -ot $WORKING_DIR/soft_env ]] \
-	&& /soft/environment/softenv-1.6.2/bin/soft-msc "$WORKING_DIR/soft_env"
+	&& HOME='' /soft/environment/softenv-1.6.2/bin/soft-msc "$WORKING_DIR/soft_env"
 source "$WORKING_DIR/soft_env.cache.sh"
 source "$WORKING_DIR/env.sh"
 
